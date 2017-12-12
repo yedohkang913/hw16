7aa7bccee93959c57a17851a649ec0de98844af9
Jeremy Sharapov <jsharapov@stuy.edu>
2017-12-12 10:57:07 -0500

    Started

--- pipe_networking.c
+++ pipe_networking.c
@@ -5,12 +5,20 @@
   server_handshake
   args: int * to_client
 
-  Perofrms the client side pipe 3 way handshake.
+  Performs the client side pipe 3 way handshake.
   Sets *to_client to the file descriptor to the downstream pipe.
 
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
+  char * toClnt = "/toClnt";
+  mkfifo(toClnt, 0666);
+  char toSend[512];
+  while (1){
+    int fd = open(toClnt, O_WRONLY);
+    fgets(toSend, 512, stdin);
+    write(fd, toSend, strlen(toSend) + 1);
+    close(fd);
   return 0;
 }
 
@@ -19,11 +27,14 @@ int server_handshake(int *to_client) {
   client_handshake
   args: int * to_server
 
-  Perofrms the client side pipe 3 way handshake.
+  Performs the client side pipe 3 way handshake.
   Sets *to_server to the file descriptor for the upstream pipe.
 
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
+  char * toClnt = "/toClnt";
+  mkfifo(toClnt, 0666);
+  char toRcv[512];
   return 0;
 }
