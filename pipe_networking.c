aa81a5b3b93c0542c4f38d521343dce568575079
Jeremy Sharapov <jsharapov@stuy.edu>
2017-12-13 10:58:22 -0500

    Did stuff

--- pipe_networking.c
+++ pipe_networking.c
@@ -12,13 +12,31 @@
   =========================*/
 int server_handshake(int *to_client) {
   char * toClnt = "/toClnt";
+  char * toSrv = "/toSrv";
+  char * toChck = "/toChck";
   mkfifo(toClnt, 0666);
-  char toSend[512];
+  mkfifo(toSrv, 0666);
+  mkfifo(toChck, 0666);
+  char toSend[512], toRcv[512];
   while (1){
-    int fd = open(toClnt, O_WRONLY);
-    fgets(toSend, 512, stdin);
-    write(fd, toSend, strlen(toSend) + 1);
-    close(fd);
+    int fd1 = open(toClnt, O_WRONLY);
+    //fgets(toSend, 512, stdin);
+    strcpy(toSend, PIN);
+    write(fd1, toSend, strlen(toSend) + 1);
+    close(fd1);
+    
+    int fd2 = open(toSrv, O_RDONLY);
+    read(fd2, toRcv, sizeof(toRcv));
+    close(fd2);
+
+    if (strcmp(toRcv, PIN) == 0){
+      int fd3 = open(toChck, O_WRONLY);
+      //fgets(toSend, 512, stdin);
+      // toSend = PIN;
+      write(fd3, toSend, strlen(toSend) + 1);
+      close(fd3);
+    }
+  }
   return 0;
 }
 
@@ -34,7 +52,32 @@ int server_handshake(int *to_client) {
   =========================*/
 int client_handshake(int *to_server) {
   char * toClnt = "/toClnt";
+  char * toSrv = "/toSrv";
+  char * toChck = "/toChck";
   mkfifo(toClnt, 0666);
-  char toRcv[512];
+  mkfifo(toSrv, 0666);
+  mkfifo(toChck, 0666);
+  char toRcv[512], toSend[512], toCheck[512];
+  while(1){
+    int fd1 = open(toClnt, O_RDONLY);
+    read(fd1, toRcv, sizeof(toRcv));
+    close(fd1);
+	 
+    if (strcmp(toRcv, PIN) == 0){
+      int fd2 = open(toSrv, O_WRONLY);
+      //fgets(toSend, 512, stdin);
+      strcpy(toSend, PIN);
+      write(fd2, toSend, strlen(toSend) + 1);
+      close(fd2);
+      
+      int fd3 = open(toChck, O_RDONLY);
+      read(fd3, toCheck, sizeof(toCheck));
+      close(fd3);
+
+      if (strcmp(toCheck, PIN) == 0){
+	printf("Handshake successful!\n");
+      }
+    }
+  }
   return 0;
 }
--- pipe_networking.h
+++ pipe_networking.h
@@ -10,6 +10,7 @@
 #ifndef NETWORKING_H
 #define NETWORKING_H
 #define ACK "HOLA"
+#define PIN "1234"
 
 #define HANDSHAKE_BUFFER_SIZE 10
 #define BUFFER_SIZE 1000
